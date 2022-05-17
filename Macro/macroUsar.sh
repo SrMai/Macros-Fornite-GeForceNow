f95df4f438c4939f547ae84f4ccf30a9fa7e9c00
Carlos Ayala <49286868+SrMai@users.noreply.github.com>
2022-05-16 19:49:03 -0500

    .
    .

--- Macro/macroUsar.sh
+++ Macro/macroUsar.sh
@@ -1,7 +1,324 @@
 #!/bin/bash
 
 while true; do
-	xdotool key e
-	xdotool click 1
-	sleep 1 
+	sleep 5
+	echo "[+] -------- Iniciando proceso -------- [+]"
+        echo "[+] Saliendo"
+        xdotool mousemove 1883 87 click 1
+        sleep 1
+	echo "[+] Abriendo inf de gremio"
+	xdotool mousemove 1379 575 click 1
+	sleep 1
+	echo "[+] Click en vista de gremio"
+	xdotool mousemove 1847 192 click 1
+	sleep 1
+	echo "[+] Mirando regalos de bonificacion"
+	xdotool mousemove 1683 251 click 1
+        sleep 1
+	xdotool mousemove 1786 171 click 1
+        sleep 1
+	echo "[+] Cobrando regalos de bonificacion"
+	xdotool mousemove 1661 171 click 1
+        sleep 1
+        echo "[+] Mirando regalos de gremio"
+        xdotool mousemove 1512 241 click 1
+        sleep 1
+        xdotool mousemove 1786 171 click 1
+        sleep 1
+        echo "[+] Cobrando regalos de gremio"
+        xdotool mousemove 1661 171 click 1
+        sleep 1
+	xdotool mousemove 1883 87 click 1
+        sleep 1
+	echo "[+] Salir a vista de castillo"
+        xdotool mousemove 1883 87 click 1
+        sleep 1
+        echo "[+] Entrando a seccion papiros"
+        xdotool mousemove 1547 591 click 1
+        sleep 1
+        echo "[+] Click en 10 mision admin"
+        xdotool mousemove 1400 165 click 1
+        sleep 1
+	echo "[+] Da 10 clicks"
+        xdotool mousemove 1788 268 click --repeat 12 --delay 500 1
+        sleep 1
+	echo "[+] Cambia a misiones de gremio"
+        xdotool mousemove 1586 172 click 1
+        sleep 1
+	echo "[+] Click en 8 misiones de gremio"
+	xdotool mousemove 1788 268 click --repeat 12 --delay 500 1
+        sleep 1
+        echo "[+] Click en Misión VIP"
+	xdotool mousemove 1758 165 click 1
+	sleep 1
+	echo "[+] Cobrando cofres"
+	xdotool mousemove 1171 333 click 1
+	sleep 1
+        xdotool mousemove 1406 333 click 1
+        sleep 1
+        xdotool mousemove 1646 333 click 1
+        sleep 1
+        xdotool mousemove 1171 450 click 1
+        sleep 1
+        xdotool mousemove 1406 450 click 1
+        sleep 1
+        xdotool mousemove 1646 450 click 1
+        sleep 1
+	echo "[+] Saliendo"
+        xdotool mousemove 1883 87 click 1
+	sleep 1
+	echo "[+] Click en caja misteriosa"
+	xdotool mousemove 1820 464 click 1
+	slepp 1
+	echo "[+] Cobrando caja misteriosa"
+	xdotool mousemove 1448 482 click 1
+	sleep 1
+	echo "[+] Click en estatua"
+	xdotool mousemove 1240 388 click 1
+	sleep 4
+	echo "[+] click en ultima etapa"
+	xdotool mousemove 1713 247 click 1
+	sleep 1
+	echo "[+] gastando res"
+	xdotool mousemove 1800 360 click 1
+	sleep 2
+	xdotool mousemove 1870 80 click 1
+	sleep 2
+        xdotool mousemove 1800 360 click 1
+        sleep 1
+        xdotool mousemove 1870 80 click 1
+        sleep 1
+        xdotool mousemove 1800 360 click 1
+        sleep 1
+        xdotool mousemove 1870 80 click 1
+        sleep 1
+	echo "[+] Saliendo"
+        xdotool mousemove 1883 87 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 4
+	echo "[+] Entrando a barco de carga"
+	xdotool mousemove 1240 460 click 1
+	sleep 1
+	echo "[+] Haciendo cambios"
+	xdotool mousemove 1190 380 click 1
+	sleep 1
+        xdotool mousemove 1645 380 click 1
+        sleep 1
+        xdotool mousemove 1190 543 click 1
+        sleep 1
+        xdotool mousemove 1645 543 click 1
+        sleep 1
+	echo "[+] Saliendo"
+        xdotool mousemove 1883 87 click 1
+        sleep 1
+	echo "[+] Entrando a coliseo"
+	xdotool mousemove 912 371 click 1
+	sleep 1
+	echo "[+] Cobrando gemas"
+	xdotool mousemove 1210 400 click 1
+	sleep 1
+	xdotool mousemove 1400 480 click 1
+	sleep 1
+	echo "[+] Ciclo de 15 peleas en proceso"
+	echo "[+] Seleccionando oponente"
+	echo "[+] Combate 1"
+	xdotool mousemove 1773 380 click 1
+	sleep 1
+	xdotool mousemove 1741 480 click 1
+	sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+	xdotool mousemove 1883 570 click 1
+	sleep 5
+	xdotool mousemove 1100 567 click 1
+	sleep 1
+        echo "[+] Combate 2"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 3"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 4"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 5"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 6"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 7"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 8"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 9"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 10"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 11"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 12"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 13"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 14"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+        echo "[+] Combate 15"
+        xdotool mousemove 1773 380 click 1
+        sleep 1
+        xdotool mousemove 1741 480 click 1
+        sleep 1
+        xdotool mousemove 1883 87 click 1
+        sleep 5
+        xdotool mousemove 1300 300 click 1
+        sleep 5
+        xdotool mousemove 1883 570 click 1
+        sleep 5
+        xdotool mousemove 1100 567 click 1
+        sleep 1
+	echo "[+] Saliendo"
+        xdotool mousemove 1883 87 click 1
+        sleep 1
+	echo "[+] -------- Repitiendo proceso -------- [+]"
 done
